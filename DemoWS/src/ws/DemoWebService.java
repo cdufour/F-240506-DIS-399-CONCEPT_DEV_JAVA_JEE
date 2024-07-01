487e6a53debec0908d1bd2beff0761ade29644e1
cdufour <opusidea@gmail.com>
2024-07-01 15:30:24 +0200

    exo1

--- DemoWS/src/ws/DemoWebService.java
+++ DemoWS/src/ws/DemoWebService.java
@@ -1,12 +1,20 @@
 package ws;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService(serviceName="demo")
 public class DemoWebService {
 
+	@WebMethod(operationName="test")
 	public String Test() {
 		return "Test";
+	}
+	
+	@WebMethod(operationName="convert")
+	public double convertInFr(@WebParam(name="amt")double amount) {
+		return amount * 6.5;
 	}
 	
 }
