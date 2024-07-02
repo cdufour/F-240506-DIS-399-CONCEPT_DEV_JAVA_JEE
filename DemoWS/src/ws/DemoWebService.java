568cdb6a89caea6ccce09f676f40e2ccee5b544a
cdufour <opusidea@gmail.com>
2024-07-02 13:28:07 +0200

    tp

--- DemoWS/src/ws/DemoWebService.java
+++ DemoWS/src/ws/DemoWebService.java
@@ -1,5 +1,6 @@
 package ws;
 
+import java.util.Random;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -16,5 +17,42 @@ public class DemoWebService {
 	public double convertInFr(@WebParam(name="amt")double amount) {
 		return amount * 6.5;
 	}
+	
+	@WebMethod(operationName="randint")
+	public int getRandInt(
+			@WebParam(name="min")int min, 
+			@WebParam(name="max")int max) 
+	{
+		Random r = new Random();
+		return r.nextInt((max - min) + 1) + min;
+	
+	}
+	
+	@WebMethod(operationName="randstr")
+	public String getAlphaNumericString(
+			@WebParam(name="len")int n) { 
+	 
+	  // choose a Character random from this String 
+	  String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	         + "0123456789"
+			 + "/-?_"
+	         + "abcdefghijklmnopqrstuvxyz"; 
+	 
+	  // create StringBuffer size of AlphaNumericString 
+	  StringBuilder sb = new StringBuilder(n); 
+	 
+	  for (int i = 0; i < n; i++) { 
+	 
+	   // generate a random number between 
+	   // 0 to AlphaNumericString variable length 
+	   int index = (int)(AlphaNumericString.length() * Math.random()); 
+	 
+	   // add Character one by one in end of sb 
+	   sb.append(AlphaNumericString 
+	      .charAt(index)); 
+	  } 
+	 
+	  return sb.toString(); 
+	 } 
 	
 }
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 - [Web Services SOAP avec Jax WS... | youtube](https://youtu.be/RFqxP4sKof0?si=fXsVIxUoHgQguq4I)
 - [Java SE 8 Archive Downloads (JDK 8u202 and earlier)](https://www.oracle.com/fr/java/technologies/javase/javase8-archive-downloads.html)
 - [Server Option is not Showing in Eclipse](https://youtu.be/ZCSn1qz-XYc?si=I0L-k0FvtQmUi95A)
+- [Jersey (framework)](https://fr.wikipedia.org/wiki/Jersey_(framework))
+- [Developing RESTful APIs with JAX-RS | youtube](https://youtube.com/playlist?list=PLqq-6Pq4lTTZh5U8RbdXq0WaYvZBz2rbn&si=N7TrVftf36HS-I9d)
